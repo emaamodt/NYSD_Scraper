501b0503d80c94add9f21d2d768cf768061e9aa0
emaamodt <emaamodt@gmail.com>
2014-02-20 16:03:11 -0800

    Added code to open Judges' pages and collect document dates.

--- NYSD_District_Judge_Scraper.rb
+++ NYSD_District_Judge_Scraper.rb
@@ -5,8 +5,14 @@ require 'open-uri'
 BASE_URL = 'http://www.nysd.uscourts.gov/'
 LIST_URL = 'http://www.nysd.uscourts.gov/judges/District' #URL for list of judges
 page = Nokogiri::HTML(open(LIST_URL)) #opens the judges list
-rows = page.css('div.mainblock table.judge_info tr') #code to select each judge's name
+
+rows = page.css('div.mainblock table.judge_info tr a') #code to select each judge's name
 
 rows.each do |row|
-	puts row.text #prints out each judge's name
+	judge = row.text #prints out each judge's name
+	judge_page = Nokogiri::HTML(open("#{BASE_URL}/#{row['href']}"))
+	dates =  judge_page.css('div.mainblock table tr td.pdf_list span.pdf_date')
+	puts judge 
+	puts dates.text
+	puts "--------------------------------------"
 end
